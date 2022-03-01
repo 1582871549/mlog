eaad10f349a78ac36259e86731e84df90c68e63e
dujianwei.meng <dujianwei.meng@bytedance.com>
2022-03-01 19:01:50 +0800

    feat(config): 配置log4j2, profiles. 拦截所有非/user前缀的请求

--- src/main/java/com/meng/mlog/common/filter/LoginFilter.java
+++ src/main/java/com/meng/mlog/common/filter/LoginFilter.java
@@ -2,20 +2,27 @@ package com.meng.mlog.common.filter;
 
 import com.meng.mlog.common.result.enums.ResultCodeEnum;
 import com.meng.mlog.common.result.helper.ExceptionHelper;
+import lombok.extern.slf4j.Slf4j;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class LoginFilter implements Filter {
+@Slf4j
+public class LoginFilter extends HttpFilter {
+
+    private static final String URL = "/user";
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        throw ExceptionHelper.createBusinessException(ResultCodeEnum.FAILED, "LoginFilter");
-        // chain.doFilter(request, response);
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("url: {}", request.getRequestURI());
+        if (request.getRequestURI().startsWith(URL)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        throw ExceptionHelper.createBusinessException(ResultCodeEnum.FAILED, "非法请求");
     }
-
 }
